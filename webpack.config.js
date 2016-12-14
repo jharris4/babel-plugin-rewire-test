a9221115bc33e769000a5f723a6a34b21c23dd57
jharris4 <harris.jb@gmail.com>
2016-12-14 17:36:16 -0500

    Add scripts & webpack config to allow bundles to be created for regular vs test usage

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,21 @@ var publicPath = '/static/';
 var outputPath = 'dist';
 var bundleJSName = '[name]-bundle.js';
 
-var isTest = process.BABEL_ENV === 'test';
+var isTest = process.env.BABEL_ENV === 'test';
 var isProd = process.env.NODE_ENV === 'production';
 
-// TODO - see if vendors can come from package.json
+console.log('webpack running, isProd? ' + isProd + ' isTest: ' + isTest);
+
 module.exports = {
-  devtool: isDev ? 'cheap-module-source-map' : 'source-map',
-  entry: {
-    main: getEntry(['./src/index']),
-    vendors: ['babel-polyfill', 'react', 'react-dom']
-  },
+  devtool: isProd ? 'source-map' : 'cheap-module-source-map',
+  entry: getEntry(),
   output: {
     path: path.join(__dirname, outputPath),
     filename: bundleJSName,
     chunkFileName: bundleJSName,
     publicPath: publicPath
   },
-  plugins: getPlugins([
-    new webpack.optimize.CommonsChunkPlugin('vendors', bundleJSName)
-  ]),
+  plugins: getPlugins(),
   module: {
     loaders: getLoaders([
       {
@@ -32,7 +28,7 @@ module.exports = {
         query: {
           cacheDirectory: true
         },
-        include: path.resolve(__dirname, 'src')
+        include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'tests')]
       }
     ])
   },
@@ -43,11 +39,23 @@ function getLoaders(loaders) {
   return loaders;
 }
 
-function getEntry(entries) {
-  return entries;
+function getEntry() {
+  if (isTest) {
+    return {
+      maintestable: ['./src/index'],
+      tests: ['./tests/index.js']
+    };
+  }
+  else {
+    return {
+      main: ['./src/index'],
+      vendors: ['babel-polyfill', 'react', 'react-dom']
+    };
+  }
 }
 
-function getPlugins(plugins) {
+function getPlugins() {
+  var plugins = isTest ? [] : [new webpack.optimize.CommonsChunkPlugin('vendors', bundleJSName)];
   if (isProd) {
     plugins.push(
       new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
